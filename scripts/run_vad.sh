801013a1c1c870a006db565a06ac13933a500acd
Victor Castellon <victor.castellon@estudiantat.upc.edu>
2022-10-24 15:54:27 +0200

    Foto esquema

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+alfa1=${1:-1}
+alfa2=${2:-20}
+
 # Be sure that this file has execution permissions:
 # Use the nautilus explorer or chmod +x run_vad.sh
 
 # Write here the name and path of your program and database
 DIR_P2=$HOME/PAV/P2
 DB=$DIR_P2/db.v4
-CMD="$DIR_P2/bin/vad --alfa1=6"
+CMD="$DIR_P2/bin/vad --alfa1=$alfa1 --alfa2=$alfa2"
 
 for filewav in $DB/*/*wav; do
 #    echo
